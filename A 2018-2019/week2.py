c05d575be8be8d2ddc0f4cb5cd95f4e02783121a
bnajafi <b.najafi87@gmail.com>
2018-10-04 16:17:00 +0200

    updating !

--- A 2018-2019/week2.py
+++ A 2018-2019/week2.py
@@ -40,7 +40,7 @@ R2 = {"name":"R_brick", "type":"cond", "length":0.1,
 Ressitance_R1 = R1["length"]/(R1["conductivity"]*R1["area"])
 R1["resistance"]= Ressitance_R1
 
-ResistanceList = [R1,R2] # A list of dictionaries
+ResistanceList = [R1,R2,Ri] # A list of dictionaries
 
 Ri = {"name":"R_i", "type":"conv", "h":9.2,"area": 15 }
 fancyResults ={"Rtot":0, "R_foam":.0003}
@@ -62,4 +62,28 @@ for anyResistance in ResistanceList:
         print("Pay attentiona that, this resistance does not have a valid type")
         print(anyResistance["name"])
 fancyResults["Rtot"] = Rtot    
+
+
+# LEt's see how we can use every thing we have learned today
+#to solve the Example 1 in simplified Wall Calculations lesson
+
+ThermalResDict = {"FaceBrick":{"R":0.075, "length":0.1}
+, "woodStud_90mm":{"R":0.36, "length":0.09}
+, "woodFiberBoard":{"R":0.23, "length":0.013}
+, "woodLappedSiding":{"R":0.14, "length":0.013}
+, "gypsum":{"R":0.079, "length":0.013}
+
+, "insideSurface":{"R":0.12}
+, "outsideSurfaceWinter":{"R":0.03}
+, "outsideSurfaceSummer":{"R":0.044}
+}
     
+R_6 = {"name":"Gypsum Wallboard","type":"cond","material":"gypsum", "length":0.013}
+R_2 = {"name":"wood bevel lapped Siding","type":"cond","material":"woodLappedSiding", "length":0.013}
+
+
+
+ResistanceList_withWood = []
+
+
+
