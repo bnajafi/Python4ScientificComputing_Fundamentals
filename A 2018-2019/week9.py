711e1119451fe9a13b8da2b62f8fdbcd14d86bf4
bnajafi <b.najafi87@gmail.com>
2018-11-22 12:33:58 +0100

    updating

--- A 2018-2019/week9.py
+++ A 2018-2019/week9.py
@@ -146,6 +146,36 @@ plt.xticks(items1,labels)
 #: How to make pie Charts!
 
 
+#pie chart:
+slices = [7,2,2,13]
+activities = ['sleeping','eating','working','playing']
+cols = ['c','m','r','b']
+
+plt.pie(slices,
+        labels=activities,
+        colors=cols,
+        startangle=90,
+        shadow= True,
+        explode=(0,0.1,0,0),
+        autopct='%1.1f%%')
+
+plt.title('Interesting Graph\nCheck it out')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
